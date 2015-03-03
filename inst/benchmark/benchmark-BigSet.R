2589279a595310b6f4e37e902a76349852f27115
mannau <mario.annau@gmail.com>
2015-03-03 17:57:40 +0100

    Adjust benchmark-BigSet script

--- inst/benchmark/benchmark-BigSet.R
+++ inst/benchmark/benchmark-BigSet.R
@@ -1,3 +1,5 @@
+### Benchmark of save/read times vs save/load
+
 require(h5)
 require(testthat)
 fname <- "test.h5"
@@ -5,42 +7,61 @@ fname <- "test.h5"
 if(file.exists(fname)) file.remove(fname)
 fname.rda <- "test.rda"
 if(file.exists(fname.rda)) file.remove(fname.rda)
-file <- H5File(fname, "a")
 
 dates <- seq(as.POSIXct("2005-01-01"), as.POSIXct("2015-01-01"), by = "mins")
 dat <- cbind(unclass(dates), 
     matrix(as.numeric(seq(length.out = length(dates)*4)), ncol = 4))
 
-time.saveh5 <- system.time(    
-    file["/bigset", "bigset", 
+if(file.exists(fname)) file.remove(fname)
+time.saveh5 <- system.time({   
+	file <- H5File(fname, "a")
+	file["/bigset", "bigset", 
         chunksize = c(nrow(dat), 1), 
         compression = 6] <- dat
-)
+	closeh5(file)
+})
 
+if(file.exists(fname.rda)) file.remove(fname.rda)
 time.saverda <- system.time(    
     save(dat, file = fname.rda, compress = "gzip", compression_level = 6)
 )
 
-cbind(time.saveh5, time.saverda)
+benchmark.save <- cbind(time.saveh5, time.saverda)
+#            time.saveh5 time.saverda
+# user.self         0.35        20.39
+# sys.self          0.07         0.11
+# elapsed           0.75        20.51
 
 time.subseth5 <- system.time({
+	  file <- H5File(fname, "r")
       vec <- seq(1, nrow(dat), by = 60)
-      out.subseth5 <- file["/bigset", "bigset"][vec, 2]
+      dset <- file["/bigset", "bigset"]
+	  out.subseth5 <- dset[vec, 2]
+	  closeh5(dset)
+	  closeh5(file)
     })
 
+# read entire set
 time.subseth52 <- system.time({
+	  file <- H5File(fname, "r")
       vec <- seq(1, nrow(dat), by = 60)
-      out.subseth52 <- file["/bigset", "bigset"][]
+	  dset <- file["/bigset", "bigset"]
+      out.subseth52 <- dset[]
       out.subseth52 <- out.subseth52[vec, 2, drop = FALSE]
+	  closeh5(dset)
+	  closeh5(file)
     })
 
 # use hyperslabs
 time.subseth53 <- system.time({
+	  file <- H5File(fname, "r")
       vec <- seq(1, nrow(dat), by = 60)
       dset <- file["/bigset", "bigset"]
       out.subseth53 <- readDataSet(dset, 
           selectDataSpace(dset, offset = c(1, 2), count = c(nrow(dat), 1)))
       out.subseth53 <- out.subseth53[vec,, drop = FALSE]
+	  closeh5(dset)
+	  closeh5(file)
     })
 
 time.subsetrda <- system.time({
@@ -48,10 +69,15 @@ time.subsetrda <- system.time({
       out.subsetrda <- dat[vec, 2, drop = FALSE]
     })
 
+file.remove(fname)
+file.remove(fname.rda)
+
 identical(out.subseth5, out.subseth52)
 identical(out.subseth5, out.subseth53)  
 identical(out.subseth5, out.subsetrda)  
 
-cbind(time.subseth5, time.subseth52, time.subseth53, time.subsetrda)
-
-closeh5(file)
+benchmark.read <- cbind(time.subseth5, time.subseth52, time.subseth53, time.subsetrda)
+#            time.subseth5 time.subseth52 time.subseth53 time.subsetrda
+# user.self          15.97           0.42           0.02           0.67
+# sys.self            0.01           0.11           0.04           0.06
+# elapsed            15.99           0.53           0.05           0.73
