5e3c8d05c578d94e42248f73883f6db08f1d94eb
mario <mario.annau@gmail.com>
2015-03-06 00:51:31 +0100

    Include windows support for zlib compression
    - Re-compile static windows libraries
    - Include zlib from local320 library collection
    - Adjust Makevars linker order

--- inst/include/zlib/zconf.h
+++ inst/include/zlib/zconf.h
@@ -408,11 +408,12 @@ typedef uLong FAR uLongf;
    typedef unsigned long z_crc_t;
 #endif
 
-#ifdef HAVE_UNISTD_H    /* may be set to #if 1 by ./configure */
+/* Mingw-w64 has unistd.h but omits including io.h which it needs */
+#if 0    /* was set to #if 1 by ./configure */
 #  define Z_HAVE_UNISTD_H
 #endif
 
-#ifdef HAVE_STDARG_H    /* may be set to #if 1 by ./configure */
+#if 1    /* was set to #if 1 by ./configure */
 #  define Z_HAVE_STDARG_H
 #endif
 
