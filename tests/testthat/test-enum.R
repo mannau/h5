1b9dd71874d9d27d9565dce178637bd6f2f29da2
mario <mario.annau@gmail.com>
2016-05-08 11:06:37 +0200

    Fix testcase DataSet-Enum, include testdata

--- tests/testthat/test-enum.R
+++ tests/testthat/test-enum.R
@@ -1,18 +1,13 @@
-context("DataSet-Enum-Vector")
+context("DataSet-Enum")
 
-# Use the sample file for the enum datatype from the HDF Group itself.
-fname <- "h5ex_t_enum.h5"
-download.file("https://www.hdfgroup.org/ftp/HDF5/examples/files/exbyapi/h5ex_t_enum.h5",
-              fname, quiet = TRUE)
-
-test_that("DataSet-Enum-Vector-open", {
+test_that("DataSet-Enum-Matrix-open", {
+    # Use the sample file for the enum datatype from the HDF Group itself,
+    # see also https://www.hdfgroup.org/ftp/HDF5/examples/files/exbyapi
+    fname <- system.file("h5ex_t_enum.h5", package = "h5", mustWork = TRUE)
     datafile <- h5file(fname, "r")
     dataset <- datafile["DS1"][]
-
     expect_that(is.factor(dataset), is_true())
     expect_that(levels(dataset), equals(c("SOLID", "LIQUID", "GAS", "PLASMA")))
-
     h5close(datafile)
 })
 
-file.remove(fname)
