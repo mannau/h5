64d92e66f245d3baef63d00ba2b8fdaed54407ef
mario <mario.annau@gmail.com>
2017-05-12 19:51:29 +0200

    Update .travis file

--- travis_setup.sh
+++ travis_setup.sh
@@ -4,8 +4,8 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then # use homebrew version
   brew install homebrew/science/hdf5
 else # install from source
   cd ..
-  if [ "$HDF5_VERSION" == "1.10.0-patch1" ]; then
-    wget "$HDF5_RELEASE_URL/hdf5-1.10/hdf5-1.10.0-patch1/src/hdf5-1.10.0-patch1.tar.gz"
+  if [ "$HDF5_VERSION" == "1.10.1" ]; then
+    wget "$HDF5_RELEASE_URL/hdf5-1.10/hdf5-1.10.1/src/hdf5-1.10.1.tar.gz
   else
     wget "$HDF5_RELEASE_URL/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
   fi
