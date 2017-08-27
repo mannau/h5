4d85058bbfaed88690ff01a1c738d0dbf9c2fc20
mannau <mario.annau@gmail.com>
2017-08-27 22:42:09 +0200

    Update travis hdf5 setup

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then # use homebrew version
-  brew install homebrew/science/hdf5
+  brew install --build-from-source hdf5 --enable-cxx
 else # install from source
   cd ..
-  if [ "$HDF5_VERSION" == "1.10.1" ]; then
-    wget "$HDF5_RELEASE_URL/hdf5-1.10/hdf5-1.10.1/src/hdf5-1.10.1.tar.gz"
-  else
-    wget "$HDF5_RELEASE_URL/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
-  fi
+  wget "$HDF5_RELEASE_URL/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
   tar -xzf "hdf5-$HDF5_VERSION.tar.gz"
   cd "hdf5-$HDF5_VERSION"
-  ./configure --prefix=/usr/local --enable-cxx --enable-build-mode=production
+  ./configure --prefix=/usr/local --enable-cxx
   sudo make install
   cd ../h5
 fi
